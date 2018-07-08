e59e7d8266d5a45f84d62bd68263be0c4735b9d6
haruyasu <harukun2002@gmail.com>
2018-07-08 10:06:06 -0400

    Week3

---
+++ tutorial/Week3/select_type.py
@@ -0,0 +1,8 @@
+selected_nodes = hou.selectedNodes()
+for node in selected_nodes:
+    node_type = node.type()
+    parent = node.parent()
+    for child_node in parent.children():
+        if child_node.type() == node_type:
+            child_node.setSelected(True)
+            
